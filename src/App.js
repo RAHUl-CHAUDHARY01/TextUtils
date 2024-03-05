1a16b477c596f2e3ac630c2a06a57ca9b3e0b9f4
Rahul Kumar <132205464+RAHUl-CHAUDHARY01@users.noreply.github.com>
2024-03-05 15:14:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import react,{ useState } from "react";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import Alert from "./components/Alert";
+import Accordion from "./components/Accordion";
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  const [mode,setMode]= useState('light');//whether dark mode is enabled or not
+  const [alert,setAlert] = useState(null);
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500);
+  }
+   const toggleMode= ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#494b4c';
+      showAlert("dark enabled","danger");
+    
+    }
+    else{
+     setMode('light');
+     document.body.style.backgroundColor = 'white';
+     showAlert("light enabled","success");
+    
+   }
+  }
+  return (
+    <>
+    <Router>
+    <Navbar title= "TextUtils"  mode={mode} toggleMode={toggleMode} abouttext="sdsds"/>
+    <Alert alert={alert} />
+    <div className="container my-3">
+    {/* <Switch>
+          <Route exact path="/about">
+            <Accordion />
+          <Route>
+          <Route path="/"> 
+          <Textform  showAlert={showAlert} heading="Enter the text to analyze below:" mode={mode} />
+          </Route>
+    </Switch>  */}
+    <Routes>
+        <Route path="/" element={<Textform  showAlert={showAlert} heading="Enter the text to analyze below:" mode={mode} />} />
+        <Route path="/accordion" element={<Accordion />} />
+         {/* <Route path="*" element={<NoMatch />} /> Catch-all route */}
+    </Routes>
+    {/* <Accordion/> */}
+    </div>
+    </Router>
+    </>
+  ); 
+}
+
+export default App;
---
+++ src/module2.mjs
@@ -0,0 +1,7 @@
+const a="rauk";
+const b="brauk";
+const c="crauk";
+const d="drauk";
+
+export default d;
+export {a,b,c};

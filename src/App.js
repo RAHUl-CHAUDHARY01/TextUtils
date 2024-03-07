e1b29f11c2a90b96f4c4f4683eb824d62c6c19a1
Rahul Kumar <132205464+RAHUl-CHAUDHARY01@users.noreply.github.com>
2024-03-08 04:44:25 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Alert from "./components/Alert";
 import Accordion from "./components/Accordion";
 import About from './components/About';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Particle from "./Particle";
 
 function App() {
   const [mode,setMode]= useState('light');//whether dark mode is enabled or not
@@ -34,9 +35,12 @@ function App() {
   }
   return (
     <>
+      <Particle />
+
     <Router>
     <Navbar title= "TextUtils"  mode={mode} toggleMode={toggleMode} abouttext="sdsds"/>
     <Alert alert={alert} />
+
     <div className="container my-3">
     {/* <Switch>
           <Route exact path="/about">
